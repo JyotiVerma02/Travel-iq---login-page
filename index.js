db72130d951f68dc6703d04aa332deaec77b3712
Jyoti Verma <jyotiverma.feb9@gmail.com>
2026-01-09 16:14:20 +0530

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let username = "Alice";
+let age = 25;
+let big = 1234567890n;
+let isHappy = true;
+let score;
+let empty = null;
+let key = Symbol("unique");
+
+
+let person = {
+    name: "Alice",
+    age: 25,
+    isStudent: false
+};
+
+
+console.log(typeof username);
+console.log(typeof age);
+console.log(typeof big);
+console.log(typeof isHappy);
+console.log(typeof score);
+console.log(typeof empty);
+console.log(typeof key);
+console.log(typeof person);
+
+
+
+
